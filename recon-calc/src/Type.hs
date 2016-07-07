9568543f9b7bb9deeec735a7d5f05aa71f03cb1b
Felix Bowman <felixgbowman@gmail.com>
2016-07-07 01:45:47 +0100

    cleanup

--- recon-calc/src/Type.hs
+++ recon-calc/src/Type.hs
@@ -62,14 +62,6 @@ infer ast = do
     constrs' <- unify constrs
     return $ applySubst constrs' tyT
 
--- parseAndInfer :: String -> ThrowsError Type
--- parseAndInfer inp = case parseExp inp of
---     Right ast -> do
---         (tyT, constrs) <- inferType ast
---         constrs' <- unify constrs
---         return $ applySubst constrs' tyT
---     Left err -> throwError $ ErrParse
-
 applySubst :: Constraints -> Type -> Type
 applySubst constr tyT = foldl' (\tyS ((TyVar name), tyC2) -> substType name tyC2 tyS) tyT constr
 
@@ -80,7 +72,7 @@ occursIn tyName ty = occin ty
         occin TyInt = False
         occin TyBool = False
         occin (TyVar s) = s == tyName
-        occin (TyProd _) = False
+        occin (TyProd x) = all occin x
 
 substConstraint :: String -> Type -> Constraints -> Constraints
 substConstraint tyName tyT constr = map (\(tyS1, tyS2) -> (substType tyName tyT tyS1, substType tyName tyT tyS2)) constr
@@ -93,6 +85,8 @@ substType tyName tyT tyS = st tyS
         st TyBool = TyBool
         st TyUnit = TyUnit
         st (x@(TyVar s)) = if s == tyName then tyT else x
+        st (TyProd tys) = TyProd (map st tys)
+        st err = error (show err)
 
 unifySubst :: Type -> Type -> Constraints -> ThrowsError [(Type, Type)]
 unifySubst ty (x@(TyVar tyName)) rest 
@@ -111,7 +105,6 @@ unify ((TyUnit, TyUnit) : rest) = unify rest
 unify ((TyBool, TyBool) : rest) = unify rest
 unify (((TyArrow tyS1 tyS2), (TyArrow tyT1 tyT2)) : rest) = unify ((tyS1, tyT1) : (tyS2, tyT2) : rest)
 unify (((TyProd tys1), (TyProd tys2)) : rest) = unify $ (zip tys1 tys2) ++ rest
--- unify (((TyVariant tys1), (TyVariant tys2)) : rest) = unify 
 unify constraints  = throwError $ ErrUnifyUnsolvable constraints
 
 insertIntoEnv :: String -> Type -> Infer Type -> Infer Type
@@ -177,7 +170,9 @@ recon (TmProj info term idx) = do
     tyT <- recon term
     simpTy <- simplify tyT
     case simpTy of
-        (TyProd tys) -> return $ tys !! idx
+        (TyProd tys) -> do
+            tell $ [(tyT, simpTy)]
+            return $ tys !! idx
         err -> throwError $ ErrNotProduct err
 recon (TmCase info term cases) = do
     tyT <- recon term
@@ -232,83 +227,3 @@ caseType fts cases = do
 isValidFields :: [(String, (String, Term))] -> [(String, Type)] -> Bool
 isValidFields cases fieldTypes = all associateTypes cases
     where associateTypes (name, (x, ty)) = name `elem` (map fst fieldTypes)
-
-
- 
--- typeOf :: TypeEnv -> Term -> Either String Type
--- typeOf env (TmTrue _) = Right TyBool
--- typeOf env (TmFalse _) = Right TyBool
--- typeOf env (TmInt _ _) = Right TyInt
--- typeOf env (TmUnit _) = Right TyUnit
--- typeOf env (TmDataDec _ _ ty) = Right TyUnit
--- typeOf env var@(TmVar info name) = getVarTy env var
--- typeOf env (TmAbs _ name ty1 t2) = do
---     let env' = Map.insert name ty1 env
---     ty2 <- typeOf env' t2
---     return $ TyArrow ty1 ty2
--- typeOf env (TmApp info t1 t2) = do
---     ty1 <- typeOf env t1
---     ty2 <- fmap (simplify env) $ typeOf env t2
---     case ty1 of
---         (TyArrow ty1' ty2') -> if ty2 == (simplify env ty1')
---             then (Right ty2') 
---             else Left $ "Param mismatch: " ++ (show ty2) ++ ", " ++ (show ty1')
---         err -> return err
--- typeOf env (TmBinOp info _ t1 t2) = do
---     if (isIntTy t1) && (isIntTy t2) 
---     then Right TyInt
---     else Left $ "Binary operator mismatch"
---         where isIntTy t = (typeOf env t == Right TyInt)
--- typeOf env (TmCase info term cases) = do
---     case fmap (simplify env) $ typeOf env term of
---         Right (TyVariant fieldTypes) -> do
---             when (not $ isValidFields cases fieldTypes) (Left "Can't find label in type")
---             caseType env fieldTypes cases
---         Right (TyRecTy x (TyVariant fieldTypes)) -> do
---             when (not $ isValidFields cases fieldTypes) (Left "Can't find label in type")
---             caseType env fieldTypes cases
---         err -> Left $ show err
--- typeOf env (TmTag info name term ty) = do
---     case simplify env ty of
---         (TyVariant fields) -> if tyTi == tyTiExpected 
---             then Right ty 
---             else Left "Field does not have expected type"
---             where (Just tyTiExpected) = name `lookup` fields
---                   (Right tyTi) = typeOf env term
---         (TyRecTy x (TyVariant fields)) -> if tyTi == tyTiExpected
---             then Right ty 
---             else Left $ "Field does not have expected type: " ++ (show tyTi) ++ ", " ++ (show tyTiExpected)
---             where (Just tyTiExpected) = name `lookup` fields
---                   (Right tyTi) = typeOf env term
---         err -> Left $ "expected variant type but got: " ++ (show err)
--- typeOf env (TmFold _ tyU tm) = return $ tyU
--- typeOf env (TmUnfold _ tyU tm) = do
---     case simplify env tyU of
---         (TyRecTy name tyT) -> typeOf (Map.insert name tyU env) tm
--- typeOf env (TmPair _ tms) = do
---     termTys <- mapM (typeOf env) tms
---     let simplified = map (simplify env) termTys
---     return $ TyProd simplified
--- typeOf env (TmProj _ t idx) = do
---     tyT <- typeOf env t
---     case simplify env tyT of
---         (TyProd tys) -> return $ tys !! idx
---         err -> Left $ "Not a product type: " ++ (show t)
--- 
--- getVarTy :: TypeEnv -> Term -> Either String Type
--- getVarTy env (TmVar info name) = case Map.lookup name env of
---     Just ty -> Right ty
---     Nothing -> Left $ "Can't find var: " ++ name ++ ", env: " ++ (show env)
--- 
--- caseType :: TypeEnv -> [(String, Type)] -> [(String, (String, Term))] -> Either String Type
--- caseType env fts cases = do
---     (tyT1 : restTys) <- mapM tyLookup cases
---     if all (== tyT1) restTys then Right tyT1 else Left "Fields do not have the same type"
---     where tyLookup (li, (xi, ti)) = do
---             let (Just tyTi) = li `lookup` fts
---             typeOf (Map.insert xi tyTi env) ti
--- 
--- isValidFields :: [(String, (String, Term))] -> [(String, Type)] -> Bool
--- isValidFields cases fieldTypes = all associateTypes cases
---     where associateTypes (name, (x, ty)) = name `elem` (map fst fieldTypes)
--- 
